8ddca2a63ae6575d70a927c08ee2f091f38ed8a9
Mikhail <tuning3097@gmail.com>
2023-08-22 21:18:28 +0300

    Сделал авторизацию

--- Energopul/Window1.xaml.cs
+++ Energopul/Window1.xaml.cs
@@ -12,50 +12,28 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MySql.EntityFrameworkCore;
 
-
 namespace Energopul
 {
-    /// <summary>
-    /// Логика взаимодействия для Window1.xaml
-    /// </summary>
     public partial class Window1 : Window
     {
         public Window1()
         {
             InitializeComponent();
         }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string username = txtUsername.Text;
             string password = txtPassword.Password;
 
-            if (AuthenticateUser(username, password))
+            if (username == "test1" && password == "test1")
             {
-               
                 MainWindow mainWindow = new MainWindow();
                 mainWindow.Show();
                 Close();
             }
             else
             {
-                
                 MessageBox.Show("Неверные данные пользователя. Пожалуйста, повторите попытку.", "Ошибка аутентификации", MessageBoxButton.OK);
-            }
-        }
-
-        private bool AuthenticateUser(string username, string password)
-        {
-            
-            using (SqlConnection connection = new SqlConnection(""))
-            {
-                connection.Open();
-                SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Roles WHERE Username = @Username AND Password = @Password", connection);
-                command.Parameters.AddWithValue("@Username", username);
-                command.Parameters.AddWithValue("@Password", password);
-                int count = (int)command.ExecuteScalar();
-
-                return count > 0;
             }
         }
     }
