82ea30474f19b08720071b6aa31fd2a50cb6df43
Mikhail <tuning3097@gmail.com>
2023-08-23 16:16:54 +0300

    Добавил Word и оптимизировал код, добавил отдельные методы на загрузку таблиц и БД

--- Energopul/MainWindow.xaml.cs
+++ Energopul/MainWindow.xaml.cs
@@ -1,32 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System;
-using System.IO;
 using System.Data;
-using System.Data.Common;
-using System.Windows;
-using System.Windows.Controls;
 using System.Data.SQLite;
-using System;
-using System.Data;
-using Path = System.IO.Path;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
 using OfficeOpenXml;
-using System;
-using System.Data;
-using System.Data.SQLite;
-using System.IO;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace Energopul
 {
@@ -38,53 +17,44 @@ namespace Energopul
             LoadDataToDataGrid();
         }
 
-        private void LoadDataToDataGrid()
+        private string GetConnectionString()
         {
             string relativePath = "energopul.db";
             string fullPath = Path.Combine(AppContext.BaseDirectory, relativePath);
-            string connectionString = $"Data Source={fullPath};Version=3;";
-            using var connection = new SQLiteConnection(connectionString);
-            connection.Open();
-        
-            const string query = "SELECT * FROM Contracts;";
-            using var command = new SQLiteCommand(query, connection);
-            using var adapter = new SQLiteDataAdapter(command);
-            var dataTable = new DataTable();
-            adapter.Fill(dataTable);
-            Table.ItemsSource = dataTable.DefaultView;
+            return $"Data Source={fullPath};Version=3;";
         }
-        
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+
+        private DataTable GetDataFromSqLite(string query)
         {
-            string relativePath = "energopul.db";
-            string fullPath = Path.Combine(AppContext.BaseDirectory, relativePath);
-            string connectionString = $"Data Source={fullPath};Version=3;";
-            using var connection = new SQLiteConnection(connectionString);
-            connection.Open();
+            using (var connection = new SQLiteConnection(GetConnectionString()))
             {
-                string query = "SELECT * FROM Contracts";
-                using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(query, connection))
-                {
-                    DataTable dataTable = new DataTable();
-                    adapter.Fill(dataTable);
-                    
-                    ExportDataTableToExcel(dataTable, "Contracts.xlsx");
-                }
+                connection.Open();
+                using var command = new SQLiteCommand(query, connection);
+                using var adapter = new SQLiteDataAdapter(command);
+                var dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                return dataTable;
             }
         }
-        static void ExportDataTableToExcel(DataTable dataTable, string fileName)
+
+        private void LoadDataToDataGrid()
+        {
+            const string query = "SELECT * FROM Contracts;";
+            DataTable dataTable = GetDataFromSqLite(query);
+            Table.ItemsSource = dataTable.DefaultView;
+        }
+
+        private void ExportDataTableToExcel(DataTable dataTable, string fileName)
         {
             using (ExcelPackage package = new ExcelPackage(new FileInfo(fileName)))
             {
                 ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Sheet1");
 
-                // Запись заголовков
                 for (int col = 0; col < dataTable.Columns.Count; col++)
                 {
                     worksheet.Cells[1, col + 1].Value = dataTable.Columns[col].ColumnName;
                 }
 
-                // Запись данных
                 for (int row = 0; row < dataTable.Rows.Count; row++)
                 {
                     for (int col = 0; col < dataTable.Columns.Count; col++)
@@ -95,6 +65,50 @@ namespace Energopul
 
                 package.Save();
             }
+        }
+        
+        private void ExportDataTableToWord(DataTable dataTable, string fileName)
+        {
+            Word.Application wordApp = new Word.Application();
+            Word.Document doc = wordApp.Documents.Add();
+            
+            Word.Paragraph para = doc.Paragraphs.Add();
+            Word.Table table = doc.Tables.Add(para.Range, dataTable.Rows.Count + 1, dataTable.Columns.Count);
+
+            for (int col = 0; col < dataTable.Columns.Count; col++)
+            {
+                table.Cell(1, col + 1).Range.Text = dataTable.Columns[col].ColumnName;
+            }
+            
+            for (int row = 0; row < dataTable.Rows.Count; row++)
+            {
+                for (int col = 0; col < dataTable.Columns.Count; col++)
+                {
+                    table.Cell(row + 2, col + 1).Range.Text = dataTable.Rows[row][col].ToString();
+                }
+            }
+
+            doc.SaveAs2(fileName);
+            doc.Close();
+            wordApp.Quit();
+
+            Marshal.ReleaseComObject(table);
+            Marshal.ReleaseComObject(para);
+            Marshal.ReleaseComObject(doc);
+            Marshal.ReleaseComObject(wordApp);
+        }
+
+        private void ExportDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "SELECT * FROM Contracts";
+            DataTable dataTable = GetDataFromSqLite(query);
+            ExportDataTableToExcel(dataTable, "Contracts.xlsx");
+        }
+        private void ExportDataToWordButton_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "SELECT * FROM Contracts";
+            DataTable dataTable = GetDataFromSqLite(query);
+            ExportDataTableToWord(dataTable, "Contracts.docx");
         }
     }
 }
